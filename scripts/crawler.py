1cadc1f8a3bb1815480fb19075008ae372d74f66
fliiiix <hi@l33t.name>
2024-01-01 17:06:20 +0100

    Update crawler script for people in feed

--- scripts/crawler.py
+++ scripts/crawler.py
@@ -8,7 +8,11 @@ PodcastFeed = feedparser.parse(url)
 
 print("Name,Date,Type,Duration,ingo,l33tname,madmas,makefu,pfleidi,marc")
 for e in PodcastFeed.entries:
-    if e.published_parsed.tm_year == 2021:
+    if e.published_parsed.tm_year == 2023:
       date_str = f"{e.published_parsed.tm_mday}.{e.published_parsed.tm_mon}.{e.published_parsed.tm_year}" 
-      print(f"\"{e.title}\",{date_str},Talk,{e.itunes_duration},0,0,0,0,0,0")
+      ingo = int("ingo" in e.podcast_person)
+      l33tname = int("l33tname" in e.podcast_person)
+      madmas = int("madmas" in e.podcast_person)
+      makefu = int("makefu" in e.podcast_person)
+      print(f"\"{e.title}\",{date_str},Talk,{e.itunes_duration},{ingo},{l33tname},{madmas},{makefu},0,0")
 
