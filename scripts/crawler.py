18da2c31a0e7881ae75f3c372c59eacb3b6ece37
fliiiix <hi@l33t.name>
2024-01-10 18:03:00 +0100

    Add recoding date to crawler

--- scripts/crawler.py
+++ scripts/crawler.py
@@ -6,13 +6,25 @@ url = "https://blog.binaergewitter.de/podcast_feed/all/mp3/rss.xml"
 
 PodcastFeed = feedparser.parse(url)
 
-print("Name,Date,Type,Duration,ingo,l33tname,madmas,makefu,pfleidi,marc")
+print(
+    "Name,Release Date,Record Date,Type,Duration,ingo,l33tname,madmas,makefu,pfleidi,marc"
+)
 for e in PodcastFeed.entries:
     if e.published_parsed.tm_year == 2023:
-      date_str = f"{e.published_parsed.tm_mday}.{e.published_parsed.tm_mon}.{e.published_parsed.tm_year}" 
-      ingo = int("ingo" in e.podcast_person)
-      l33tname = int("l33tname" in e.podcast_person)
-      madmas = int("madmas" in e.podcast_person)
-      makefu = int("makefu" in e.podcast_person)
-      print(f"\"{e.title}\",{date_str},Talk,{e.itunes_duration},{ingo},{l33tname},{madmas},{makefu},0,0")
+        release_date_str = f"{e.published_parsed.tm_year}-{e.published_parsed.tm_mon:02}-{e.published_parsed.tm_mday:02}"
 
+        # Parse Date from download file as this contains the recording date
+        # https://download.binaergewitter.de/2023-12-30.Binaergewitter.Talk.328.mp3
+        record_date_str = (
+            e.links[1]
+            .href.replace("https://download.binaergewitter.de/", "")
+            .split(".")[0]
+        )
+
+        ingo = int("ingo" in e.podcast_person)
+        l33tname = int("l33tname" in e.podcast_person)
+        madmas = int("madmas" in e.podcast_person)
+        makefu = int("makefu" in e.podcast_person)
+        print(
+            f'"{e.title}",{release_date_str},{record_date_str},Talk,{e.itunes_duration},{ingo},{l33tname},{madmas},{makefu},0,0'
+        )
