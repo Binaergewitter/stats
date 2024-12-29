5f438d7efd42bf3916c3ae5054190fa7a4b32901
Markus Schlichting <markus.schlichting@karakun.com>
2024-12-29 19:54:56 +0100

    gather data for 2024

--- scripts/crawler.py
+++ scripts/crawler.py
@@ -10,7 +10,7 @@ print(
     "Name,Release Date,Record Date,Type,Duration,ingo,l33tname,madmas,makefu,pfleidi,marc"
 )
 for e in PodcastFeed.entries:
-    if e.published_parsed.tm_year == 2023:
+    if e.published_parsed.tm_year == 2024:
         release_date_str = f"{e.published_parsed.tm_year}-{e.published_parsed.tm_mon:02}-{e.published_parsed.tm_mday:02}"
 
         # Parse Date from download file as this contains the recording date
